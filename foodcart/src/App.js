1000afda97f93d3b8c8f94a7b559c4b4f481afe7
vinay <kanniyesh2@gmail.com>
2024-08-17 21:09:20 +0530

    food

---
+++ foodcart/src/App.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import Products from './Products';
+
+const App = () => {
+  const [search,setSearch] = useState('');
+  const [data,setData] = useState([]);
+  const YOUR_APP_ID = "0acc91d1";
+  const YOUR_APP_KEY ="0af501d54ef659553a4f44ba77c19002";
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
+      response => response.json()
+    ).then(
+      data => setData(data.hits)
+    )
+  }
+  return (
+    <div className='app'>
+      <center>
+        <h4>Food Recipe App</h4><br/>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
+          <input type="submit" className="btn btn-primary" value="Search"/>
+        </form>
+        {data.length>=1 ? <Products  data={data}/>:null}
+      </center>
+    </div>
+  )
+}
+
+export default App
